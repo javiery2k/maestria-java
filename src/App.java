716a2fafc383e7169e6ce072712343eb157b2ff6
Javier <javier.moran@modop.com>
2021-05-13 21:28:43 -0500

    Layout

--- src/App.java
+++ src/App.java
@@ -4,9 +4,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+import javax.swing.JTabbedPane;
+import javax.swing.JToolBar;
 
 public class App extends JFrame {
-
+	Color color1 = new Color(234, 238, 241);
+	Color color2 = new Color(42, 88, 173);
+	Color color3 = new Color(34, 171, 233);
 	private JPanel contentPane;
 
 	/**
@@ -29,12 +40,70 @@ public class App extends JFrame {
 	 * Create the frame.
 	 */
 	public App() {
+		setTitle("Control de Estacionamientos");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1000, 550);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
 
+		JPanel panel = new JPanel();
+		panel.setBackground(color2);
+		panel.setForeground(color3);
+		panel.setBounds(0, 0, 289, 513);
+		contentPane.add(panel);
+		panel.setLayout(null);
+
+		Button button = new Button("Ingresar Vehiculo");
+		button.setBackground(Color.WHITE);
+		button.setBounds(27, 369, 115, 60);
+		panel.add(button);
+
+		Button button_1 = new Button("Retirar Vehiculo");
+		button_1.setBounds(148, 369, 115, 60);
+		panel.add(button_1);
+
+		Button button_1_1 = new Button("Listar Vehiculos");
+		button_1_1.setBounds(27, 435, 115, 60);
+		panel.add(button_1_1);
+
+		Button button_1_1_1 = new Button("Salir");
+		button_1_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+			}
+		});
+		button_1_1_1.setBounds(148, 435, 115, 60);
+		panel.add(button_1_1_1);
+
+		JLabel lblNewLabel = new JLabel("Parking v1.0");
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setFont(new Font("Century Gothic", Font.BOLD, 27));
+		lblNewLabel.setBounds(65, 87, 160, 34);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon(App.class.getResource("/assets/parking-logo.png")));
+		lblNewLabel_1.setBounds(80, 174, 128, 128);
+		panel.add(lblNewLabel_1);
+
+		JPanel panel_2 = new JPanel();
+		panel_2.setVisible(false);
+		panel_2.setBounds(286, 0, 700, 513);
+		contentPane.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JPanel panel_2_1 = new JPanel();
+		panel_2_1.setLayout(null);
+		panel_2_1.setVisible(false);
+		panel_2_1.setBounds(0, 0, 700, 513);
+		contentPane.add(panel_2_1);
+		
+		JPanel panel_2_2 = new JPanel();
+		panel_2_2.setLayout(null);
+		panel_2_2.setVisible(false);
+		panel_2_2.setBounds(0, 0, 700, 513);
+		contentPane.add(panel_2_2);
+	}
 }
--- src/Login.java
+++ src/Login.java
@@ -49,7 +49,7 @@ public class Login extends JFrame {
 		/**
 		 * Configuracion Global
 		 */
-		setTitle("Bienvenidos");
+		setTitle("Sistemas de Estacionamientos en Java");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 650, 350);
 		/**
@@ -59,7 +59,7 @@ public class Login extends JFrame {
 		contentPane.setBorder(null);
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		/**
 		 * Jpanel Izq
 		 */
@@ -69,7 +69,7 @@ public class Login extends JFrame {
 		panel.setBounds(0, 0, 325, 313);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		/**
 		 * Jpanel Derecha
 		 */
@@ -80,38 +80,43 @@ public class Login extends JFrame {
 		panel_1.setBounds(324, 0, 312, 313);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
-		
+
 		/**
 		 * Etiquetas
 		 */
-		JLabel lblNewLabel_2 = new JLabel("AUTOMOTRIZ");
+		JLabel lblNewLabel_2 = new JLabel("Parking 1.0");
+		lblNewLabel_2.setFont(new Font("Century Gothic", Font.PLAIN, 16));
 		lblNewLabel_2.setForeground(color2);
-		lblNewLabel_2.setFont(new Font("SansSerif", Font.PLAIN, 18));
-		lblNewLabel_2.setBounds(104, 239, 119, 24);
+		lblNewLabel_2.setBounds(120, 259, 84, 21);
 		panel.add(lblNewLabel_2);
 
-		JLabel lblNewLabel_2_1 = new JLabel("SISTEMA DE INVENTARIO");
+		JLabel lblNewLabel_2_1 = new JLabel("SISTEMA DE ESTACIONAMIENTOS");
+		lblNewLabel_2_1.setFont(new Font("Century Gothic", Font.PLAIN, 16));
 		lblNewLabel_2_1.setForeground(new Color(42, 88, 173));
-		lblNewLabel_2_1.setFont(new Font("SansSerif", Font.PLAIN, 18));
-		lblNewLabel_2_1.setBounds(49, 214, 225, 24);
+		lblNewLabel_2_1.setBounds(43, 234, 252, 21);
 		panel.add(lblNewLabel_2_1);
 
 		JLabel lblNewLabel_3 = new JLabel("");
-		lblNewLabel_3.setIcon(new ImageIcon(Login.class.getResource("/assets/logo.png")));
-		lblNewLabel_3.setBounds(60, 59, 200, 129);
+		lblNewLabel_3.setIcon(new ImageIcon(Login.class.getResource("/assets/parking-icon-150.png")));
+		lblNewLabel_3.setBounds(84, 59, 150, 151);
 		panel.add(lblNewLabel_3);
+
+		JLabel lblNewLabel_2_1_1 = new JLabel("Todos los derechos reservados 2021");
+		lblNewLabel_2_1_1.setForeground(new Color(42, 88, 173));
+		lblNewLabel_2_1_1.setFont(new Font("Century Gothic", Font.PLAIN, 10));
+		lblNewLabel_2_1_1.setBounds(81, 288, 173, 14);
+		panel.add(lblNewLabel_2_1_1);
 
 		/**
 		 * Mensaje de Validacion
 		 */
 		JLabel lblNewLabel_4 = new JLabel("Las Credenciales no coinciden");
+		lblNewLabel_4.setFont(new Font("Century Gothic", Font.PLAIN, 12));
 		lblNewLabel_4.setVisible(false);
-		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		lblNewLabel_4.setForeground(Color.WHITE);
 		lblNewLabel_4.setBounds(73, 186, 191, 14);
 		panel_1.add(lblNewLabel_4);
-		
-		
+
 		/**
 		 * Campo de Usuario
 		 */
@@ -122,7 +127,7 @@ public class Login extends JFrame {
 		textField.setCaretColor(Color.WHITE);
 		textField.setBounds(73, 106, 190, 20);
 		panel_1.add(textField);
-		
+
 		/**
 		 * Campo de Password
 		 */
@@ -133,64 +138,70 @@ public class Login extends JFrame {
 		passwordField.setCaretColor(Color.WHITE);
 		passwordField.setBounds(73, 154, 190, 20);
 		panel_1.add(passwordField);
-		
+
 		/**
 		 * Logos. estamos usando el path relativo a la clase
 		 */
 		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(Login.class.getResource("/assets/icons8_customer_32px_1.png")));
+		lblNewLabel.setIcon(new ImageIcon(Login.class.getResource("/assets/username-icon.png")));
 		lblNewLabel.setBounds(31, 101, 32, 32);
 		panel_1.add(lblNewLabel);
 
 		JLabel lblNewLabel_1 = new JLabel("");
-		lblNewLabel_1.setIcon(new ImageIcon(Login.class.getResource("/assets/icons8_Key_32px.png")));
+		lblNewLabel_1.setIcon(new ImageIcon(Login.class.getResource("/assets/password-icon.png")));
 		lblNewLabel_1.setBounds(31, 146, 32, 32);
 		panel_1.add(lblNewLabel_1);
-		
-		
-		
+
 		/**
 		 * Boton de Entrar
 		 */
 		JButton btnNewButton = new JButton("Entrar");
+		btnNewButton.setFont(new Font("Century Gothic", Font.PLAIN, 11));
 		btnNewButton.setForeground(Color.WHITE);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// Hacemos nuestra coneccion de BD
-				AppConection conn = new AppConection();
-				
-				// Validamos las credenciales
+				// Obtenemos los valores ingresados
 				String user = textField.getText();
 				String pass = passwordField.getText();
-				
-				// Si las credenciales son validas, cerramos el login y abrimos el app
-				if (conn.logearse(user, pass)) {
-					setVisible(false);
-					
-					// Lanzamos nuestra app
-					App app = new App();
-					app.setVisible(true);
-					
-					
+
+				// Validamos las credenciales
+				if (!user.isEmpty() && !pass.isEmpty()) {
+
+					// Hacemos nuestra coneccion de BD
+					AppConection conn = new AppConection();
+
+					// Si las credenciales son validas, cerramos el login y abrimos el app
+					if (conn.logearse(user, pass)) {
+						setVisible(false);
+
+						// Lanzamos nuestra app
+						App app = new App();
+						app.setVisible(true);
+
+					} else {
+						lblNewLabel_4.setVisible(true);
+					}
 				} else {
 					lblNewLabel_4.setVisible(true);
 				}
+
 			}
 		});
 		btnNewButton.setBorder(new LineBorder(Color.WHITE));
 		btnNewButton.setBackground(color2);
 		btnNewButton.setBounds(73, 226, 89, 23);
 		panel_1.add(btnNewButton);
-		
+
 		/**
 		 * Boton de Salir
 		 */
 		JButton btnSalir = new JButton("Salir");
+		btnSalir.setFont(new Font("Century Gothic", Font.PLAIN, 11));
 		btnSalir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				setVisible(false);
 			}
-		});		
+		});
 		btnSalir.setBorder(new LineBorder(Color.WHITE));
 		btnSalir.setBackground(color2);
 		btnSalir.setForeground(Color.WHITE);
